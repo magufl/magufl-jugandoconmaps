3dea0fdec1b29b0f85f5b936f73439a4207a091a
Mar Aguayo Florit <154342626+magufl@users.noreply.github.com>
2024-03-25 20:35:00 +0000

    a medias

--- src/app.js
+++ src/app.js
@@ -6,37 +6,36 @@ import "./assets/img/4geeks.ico";
 
 
 window.onload = function () {
-  // Write your code here
-  console.log('hola mundo')
-
-  // Renderizar la lista de tareas en una ul en el div 'todos'
-
   // 1. Declarar el listado de tareas
   const todos = [
     {
       "name": "Jhon",
       "id": 1,
       "title": "delectus aut autem",
-      "completed": false
+      "completed": false,
+      "pic": "https://imagenes.noticiasrcn.com/ImgNoticias/meme%20del%20mono%20insatisfecho.jpeg?VersionId=PJ1PGFFyJEM6V4hrSH0Gmcprvqy0x_ck",
     },
     {
       "name": "Ringo",
       "id": 2,
       "title": "quis ut nam facilis et officia qui",
-      "completed": false
+      "completed": false,
+      "pic": "https://i.pinimg.com/236x/ef/34/86/ef34862c33c40f9ce27eaf47e8bdf973.jpg"
     },
     {
       "name": "George",
       "id": 3,
       "title": "fugiat veniam minus",
-      "completed": false
+      "completed": false,
+      "pic": "https://petslife.com.mx/wp-content/uploads/2022/08/memes-2.jpg"
     },
     {
       "name": "George",
       "id": 4,
       "title": "et porro tempora",
-      "completed": true
-    },
+      "completed": true,
+      "pic": " https://i.chzbgr.com/original/8383144448/h7DFB14D7/bromas-perros-memes-animales-8383144448"
+    },/* 
     {
       "name": "Jhon",
       "id": 5,
@@ -72,41 +71,32 @@ window.onload = function () {
       "id": 10,
       "title": "illo est ratione doloremque quia",
       "completed": true
-    },
+    }, */
   ]
-  console.log(todos);
-
   // 2. Declarar la variable donde voy a armar el html entero
-  let todosHTML = '<ul class="list-group">'
+  let todosHTML = ''
   // 2. Recorrer el array con un .map()
-  todos.map((item, id) => { 
-
-    todosHTML += item.completed ? '' : `
-    <div class="alert alert-${item.completed ? 'success' : 'danger'}">
-      <li class="list-group-item d-flex justify-content-between">
-            <div>
-              <span class="text-success">${item.name}</span>
-              - ${item.title}
-            </div>
-            <div>
-              ${!item.completed ? 
-                '<span class="mx-1"><i class="far fa-check-circle text-success"></i></span>'
-                : '<span class="mx-1"><i class="far fa-times-circle text-danger"></i></span>'}
-              
-            </div>
-      </li>
-    </div>
-      `
-    })
-    todosHTML += '</ul>'
-    console.log(todosHTML);
-
-
+  todos.map((item, id) => {
+    todosHTML += `
+    <div class="card" style="width: 18rem;">
+      <img src="${item.pic}" class="card-img-top img-thumbnail" style="height: 30vw; object-fit: cover" alt="${item.title}">
+      <div class="card-body">
+        <h5 class="card-title">${item.name}</h5>
+        <p class="card-text">Con el ID: ${item.id} </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div> 
+    `
+  })
+  todosHTML += ''
+  console.log(todosHTML);
 
   // n. Capturar el elemento html en una variable
-  const listado = document.querySelector('#todos')
+  const listado = document.querySelector('#pills-home')
   listado.innerHTML = todosHTML
 
-  // document.getElementById('byId').innerHTML = todosHTML
+
+ /*  const listado = document.querySelector('#pills-home')
+  listado.innerHTML = todosHTML */
 
 };
